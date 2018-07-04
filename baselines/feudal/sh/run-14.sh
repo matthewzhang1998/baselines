8c183a9889981c9ed1c0e04d49bb122569a76527
Matthew Zhang <matthew.zhang@mail.utoronto.ca>
2018-07-04 17:40:29 -0400

    no print with params

---
+++ baselines/feudal/sh/run-14.sh
@@ -0,0 +1,2 @@
+python run_program.py --env NumSwap-v0 --nsteps 10000 --tsteps 10000000 --seed 1 --gmax 0.001 --quiet 1 --cliphigh 0.2 --stoch 0.0 --bmax 0.25 --ngmin 8 --recurrent 0 --nhier 2 --maxlen 20 --log test/iter-14
+python run_program.py --env NumSwap-v0 --nsteps 10000 --tsteps 10000000 --seed 1 --gmax 0.001 --quiet 1 --cliphigh 0.2 --stoch 0.0 --bmax 0.25 --ngmin 8 --recurrent 0 --nhier 3 --maxlen 20 --log test/iter-14
